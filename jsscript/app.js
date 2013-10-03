f46fa641c6790a7a797906e685d8f99a5ea79689
Hao-Kuang Tsai <kuang.con@gmail.com>
2013-10-04 01:35:32 +0800

    test 02

--- jsscript/app.js
+++ jsscript/app.js
@@ -1,5 +1,15 @@
 document.getElementById("demo").innerHTML="Hello World";
 
+function httpGet(theUrl)
+{
+  var xmlHttp = null;
+ 
+  xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false );
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
+}
+
 app_id = '212467558922080';
 app_secret = 'eacb1a82179eba9b7af92e7e6cbcf882';
 my_url = '//rawgithub.com/Conjuror/location-analyzer/patch-001/facebook/location-analyzer/index.html';
