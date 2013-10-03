85407e87e5fa035660e0f260fb575f7f6cbe53f0
Hao-Kuang Tsai <kuang.con@gmail.com>
2013-10-04 01:58:35 +0800

    test006

--- jsscript/app.js
+++ jsscript/app.js
@@ -1,30 +1,30 @@
 document.getElementById("demo").innerHTML="Hello World";
 
-function httpGet(theUrl)
-{
-  var xmlHttp = null;
+// function httpGet(theUrl)
+// {
+//   var xmlHttp = null;
  
-  xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", theUrl, false );
-  xmlHttp.send( null );
-  return xmlHttp.responseText;
-}
+//   xmlHttp = new XMLHttpRequest();
+//   xmlHttp.open( "GET", theUrl, false );
+//   xmlHttp.send( null );
+//   return xmlHttp.responseText;
+// }
 
-app_id = '212467558922080';
-app_secret = 'eacb1a82179eba9b7af92e7e6cbcf882';
-my_url = '//rawgithub.com/Conjuror/location-analyzer/patch-001/facebook/location-analyzer/index.html';
+// app_id = '212467558922080';
+// app_secret = 'eacb1a82179eba9b7af92e7e6cbcf882';
+// my_url = '//rawgithub.com/Conjuror/location-analyzer/patch-001/facebook/location-analyzer/index.html';
 
-dialog_url = 'https://www.facebook.com/dialog/oauth?client_id=' + app_id
-             + '&redirect_uri=' + encodeURI(my_url) ;
+// dialog_url = 'https://www.facebook.com/dialog/oauth?client_id=' + app_id
+//              + '&redirect_uri=' + encodeURI(my_url) ;
 
-//auth user
-token_url = 'https://graph.facebook.com/oauth/access_token?client_id=' + app_id
-            + '&redirect_url=' + encodeURI(my_url)
-            + '&client_secret=' + app_secret
-            + '&code=';
+// //auth user
+// token_url = 'https://graph.facebook.com/oauth/access_token?client_id=' + app_id
+//             + '&redirect_url=' + encodeURI(my_url)
+//             + '&client_secret=' + app_secret
+//             + '&code=';
 
-data  = httpGet(token_url);
+// data  = httpGet(token_url);
 
-console.log(data);
+// console.log(data);
 
 
