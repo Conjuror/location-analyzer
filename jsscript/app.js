db71acbba6710dacca33f3cd18b28e24cfd385da
Hao-Kuang Tsai <kuang.con@gmail.com>
2013-10-04 01:47:40 +0800

    test004

--- jsscript/app.js
+++ jsscript/app.js
@@ -14,11 +14,14 @@ app_id = '212467558922080';
 app_secret = 'eacb1a82179eba9b7af92e7e6cbcf882';
 my_url = '//rawgithub.com/Conjuror/location-analyzer/patch-001/facebook/location-analyzer/index.html';
 
+dialog_url = 'https://www.facebook.com/dialog/oauth?client_id=' + app_id
+             + '&redirect_uri=' . urlencode($my_url) ;
+
 //auth user
 token_url = 'https://graph.facebook.com/oauth/access_token?client_id=' + app_id
             + '&redirect_url=' + encodeURI(my_url)
             + '&client_secret=' + app_secret;
-            // + '&code=["code"]';
+            + '&code=';
 
 data  = httpGet(token_url);
 
