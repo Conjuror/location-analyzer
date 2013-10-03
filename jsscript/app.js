bcb371e27dc62aa8f0a02a920d1ad97ba2f3ca96
Hao-Kuang Tsai <kuang.con@gmail.com>
2013-10-03 23:44:29 +0800

    Test if I can have the access of FB

---
+++ jsscript/app.js
@@ -0,0 +1,21 @@
+document.getElementById("demo").innerHTML="Hello World";
+window.fbAsyncInit = function() {
+  // init the FB JS SDK
+  FB.init({
+    appId      : '212467558922080',                    // App ID from the app dashboard
+    channelUrl : 'channel.html',                       // Channel file for x-domain comms
+    status     : true,                                 // Check Facebook Login status
+    xfbml      : true
+  });
+
+  // Additional initialization code such as adding Event Listeners goes here
+};
+
+// Load the SDK asynchronously
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+	    js.src = "//connect.facebook.net/en_US/all.js";
+	    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
