9d3970d252bb3153a34277f46ab4daa2b4f0a235
Hao-Kuang Tsai <kuang.con@gmail.com>
2013-10-04 01:33:53 +0800

    test query

--- jsscript/app.js
+++ jsscript/app.js
@@ -1,1 +1,17 @@
 document.getElementById("demo").innerHTML="Hello World";
+
+app_id = '212467558922080';
+app_secret = 'eacb1a82179eba9b7af92e7e6cbcf882';
+my_url = '//rawgithub.com/Conjuror/location-analyzer/patch-001/facebook/location-analyzer/index.html';
+
+//auth user
+token_url = 'https://graph.facebook.com/oauth/access_token?client_id=' + app_id
+            + '&redirect_url=' + encodeURI(my_url)
+            + '&client_secret=' + app_secret
+            + '&code=["code"]';
+
+data  = httpGet(token_url);
+
+console.log(data);
+
+
